3b8e602b437d3ec16c420c77beb8a528a2fdbc57
Onur Yılmaz <ttygtonrylmz@gmail.com>
2020-10-30 21:09:57 +0300

    Hacker Rank

---
+++ SherlockValidString/SherlockValidString/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace SherlockValidString
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+        static string isValid(string s)
+        {
+            if (!Regex.IsMatch(s, @"^[a-z]+$")) return "NO";
+
+            var dic = new Dictionary<char, int>();
+            foreach (char item in s)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item] += 1;
+                }
+                else
+                {
+                    dic[item] = 1;
+                }
+            }
+
+            int counter = 0;
+            bool IsValid = true;
+            int firstValue = dic.First().Value;
+            foreach (int value in dic.Values)
+            {
+                if (firstValue != value)
+                {
+                    if (counter >= 1)
+                    {
+                        IsValid = false;
+                        break;
+                    }
+                    counter++;
+                }
+            }
+            if (IsValid)
+                return "YES";
+            else
+                return "NO";
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            string result = isValid(Console.ReadLine());
+
+            Console.WriteLine(result);
+        }
+    }
+}
